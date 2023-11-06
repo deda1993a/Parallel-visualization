7a68a551965542c1601fb150ffa51ebeadb16d41
deda1993a <92177837+deda1993a@users.noreply.github.com>
2023-11-06 23:06:43 +0100

    Add files via upload
    1

---
+++ Parallel visualization/Form1.cs
@@ -0,0 +1,21 @@
+namespace Parallel_visualization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mergeSortToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            Form Form2 = new Form2();
+            Form2.Size=new Size(816, 489);
+          
+            Form2.Show();
+          
+            //this.Hide();
+        }
+    }
+}
