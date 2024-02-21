e9dd8ec9c0686cd5154e55fe90e03d59ef822d97
David Demeter <demeterd1993@hotmail.com>
2024-02-21 15:57:21 +0100

    02.21-2

--- Parallel visualization/Form5.cs
+++ Parallel visualization/Form5.cs
@@ -4,124 +4,142 @@ using System.Diagnostics;
 namespace Parallel_visualization
 {
 
-    public class HPoint
-    {
-        public double X { get; set; }
-        public double Y { get; set; }
 
-        public HPoint(double x, double y)
-        {
-            X = x;
-            Y = y;
-        }
 
-        public static HPoint operator -(HPoint a, HPoint b)
-        {
-            return new HPoint(a.X - b.X, a.Y - b.Y);
-        }
 
-        public static double Cross(HPoint O, HPoint A, HPoint B)
-        {
-            return (A.X - O.X) * (B.Y - O.Y) - (A.Y - O.Y) * (B.X - O.X);
-        }
-    }
-
-    class PolarComparer : IComparer<HPoint>
-    {
-        private HPoint pivot;
-
-        public PolarComparer(HPoint pivot)
-        {
-            this.pivot = pivot;
-        }
-
-        public int Compare(HPoint a, HPoint b)
-        {
-            if (a == b)
-                return 0;
-
-            double cross = HPoint.Cross(pivot, a, b);
-
-            if (cross > 0)
-                return -1;
-            if (cross < 0)
-                return 1;
-
-            double distanceA = Math.Pow(pivot.X - a.X, 2) + Math.Pow(pivot.Y - a.Y, 2);
-            double distanceB = Math.Pow(pivot.X - b.X, 2) + Math.Pow(pivot.Y - b.Y, 2);
-
-            return distanceA.CompareTo(distanceB);
-        }
-    }
 
     public partial class Form5 : Form
     {
-        public Form5()
+        /*public Form5()
         {
             InitializeComponent();
+        }*/
+
+
+        private readonly List<Point> points;
+        private readonly Button computeHullButton;
+        private readonly PictureBox pictureBox;
+        private readonly Label executionTimeLabel;
+
+        public Form5()
+        {
+            this.ClientSize = new Size(800, 600);
+
+            
+
+            computeHullButton = new Button
+            {
+                Text = "Szekveniális Konvex burok számítása",
+                Location = new Point(10, 10),
+                Size = new Size(170, 30)
+            };
+            computeHullButton.Click += ComputeHullButtonClick;
+
+            pictureBox = new PictureBox
+            {
+                Location = new Point(10, 50),
+                Size = new Size(780, 500),
+                BorderStyle = BorderStyle.Fixed3D,
+                BackColor = Color.White
+            };
+
+            executionTimeLabel = new Label
+            {
+                Location = new Point(10, 560),
+                Size = new Size(780, 30)
+            };
+
+            this.Controls.Add(computeHullButton);
+            this.Controls.Add(pictureBox);
+            this.Controls.Add(executionTimeLabel);
+            computeHullButton.Click += ComputeHullButtonClick;
+            
+            points = GeneratePoints(100); // Generate 100 random points
         }
 
-        public static List<HPoint> ComputeConvexHull(List<HPoint> pointss)
+        private void ComputeHullButtonClick(object sender, EventArgs e)
         {
-            if (pointss.Count <= 1)
-                return pointss.ToList();
-
-            // Find pivot
-            HPoint pivot = pointss[0];
-            foreach (HPoint p in pointss)
-            {
-                if (p.Y < pivot.Y || (p.Y == pivot.Y && p.X < pivot.X))
-                    pivot = p;
-            }
-
-            // Sort pointss by polar angle with pivot
-            pointss = pointss.OrderBy(p => p, new PolarComparer(pivot)).ToList();
-
-            // Divide pointss among threads
-            var partitioner = Partitioner.Create(0, pointss.Count);
-            ConcurrentBag<List<HPoint>> localHulls = new ConcurrentBag<List<HPoint>>();
-
-            Parallel.ForEach(partitioner, (range, state) =>
-            {
-                var localHull = SequentialGrahamScan(pointss.GetRange(range.Item1, range.Item2 - range.Item1));
-                localHulls.Add(localHull);
-            });
-
-            // Merge local hulls into the global hull
-            var globalHull = MergeLocalHulls(localHulls);
-
-            return globalHull;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            var hull = ComputeConvexHull(points);
+            stopwatch.Stop();
+            long microseconds = stopwatch.Elapsed.Ticks / (TimeSpan.TicksPerMillisecond / 1000);
+            DrawHull(hull, pictureBox);
+            executionTimeLabel.Text = $"Végrehajtási idő: {microseconds} μs";
         }
 
-        private static List<HPoint> SequentialGrahamScan(List<HPoint> pointss)
+        private List<Point> GeneratePoints(int count)
         {
-            if (pointss.Count <= 1)
-                return pointss;
+            Random rand = new Random();
+            return Enumerable.Range(0, count).Select(_ => new Point(rand.Next(pictureBox.Width), rand.Next(pictureBox.Height))).ToList();
+        }
 
-            Stack<HPoint> hull = new Stack<HPoint>();
-            hull.Push(pointss[0]);
-            hull.Push(pointss[1]);
+        private Stack<Point> ComputeConvexHull(List<Point> points)
+        {
+            if (points.Count < 3) return new Stack<Point>();
 
-            for (int i = 2; i < pointss.Count; i++)
+            Point lowestPoint = points.Aggregate((minPoint, nextPoint) => nextPoint.Y < minPoint.Y || (nextPoint.Y == minPoint.Y && nextPoint.X < minPoint.X) ? nextPoint : minPoint);
+            var sortedPoints = points.OrderBy(point => Math.Atan2(point.Y - lowestPoint.Y, point.X - lowestPoint.X)).ToList();
+
+            Stack<Point> hull = new Stack<Point>();
+            hull.Push(lowestPoint);
+            hull.Push(sortedPoints[0]);
+            hull.Push(sortedPoints[1]);
+
+            for (int i = 2; i < sortedPoints.Count; i++)
             {
-                HPoint top = hull.Pop();
-                while (hull.Count != 0 && HPoint.Cross(hull.Peek(), top, pointss[i]) <= 0)
+                Point top = hull.Pop();
+                while (Orientation(NextToTop(hull), top, sortedPoints[i]) != -1)
+                {
                     top = hull.Pop();
+                }
                 hull.Push(top);
-                hull.Push(pointss[i]);
+                hull.Push(sortedPoints[i]);
             }
 
-            return hull.ToList();
+            return hull;
         }
 
-        private static List<HPoint> MergeLocalHulls(ConcurrentBag<List<HPoint>> localHulls)
+        private void DrawHull(Stack<Point> hull, PictureBox pictureBox)
         {
-            // Simple hull merging, assumes local hulls are small compared to total pointss
-            // For more efficient merging, consider implementing a more sophisticated algorithm
-            var pointss = localHulls.SelectMany(h => h).Distinct().ToList();
-            pointss.Sort((a, b) => a.X == b.X ? a.Y.CompareTo(b.Y) : a.X.CompareTo(b.X));
-            return SequentialGrahamScan(pointss);
+            Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(Color.White);
+                if (hull.Count > 0)
+                {
+                    Point prev = hull.Peek(); // Start with the last point
+                    foreach (var point in hull)
+                    {
+                        g.DrawLine(Pens.Black, prev, point);
+                        prev = point;
+                    }
+                    g.DrawLine(Pens.Black, prev, hull.Peek()); // Close the hull
+                }
+
+                // Draw points
+                foreach (var point in points)
+                {
+                    g.FillEllipse(Brushes.Red, point.X - 2, point.Y - 2, 4, 4);
+                }
+            }
+            pictureBox.Image = bitmap;
         }
+
+        private static Point NextToTop(Stack<Point> stack)
+        {
+            Point top = stack.Pop();
+            Point nextToTop = stack.Peek();
+            stack.Push(top);
+            return nextToTop;
+        }
+
+        private static int Orientation(Point p, Point q, Point r)
+        {
+            int val = (q.Y - p.Y) * (r.X - q.X) - (q.X - p.X) * (r.Y - q.Y);
+            return val == 0 ? 0 : (val > 0 ? 1 : -1); // 0: collinear, 1: clockwise, -1: counter-clockwise
+        }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -159,7 +177,7 @@ namespace Parallel_visualization
         private void button2_Click(object sender, EventArgs e)
         {
            // ParallelGrahamScan(pointsss);
-
+           
         }
     }
 
